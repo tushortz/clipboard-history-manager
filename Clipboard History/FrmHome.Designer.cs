0c033e36b8ae9557158de01439c774f244592213
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-07-11 12:35:45 +0100

    Add files via upload

---
+++ Clipboard History/FrmHome.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Clipboard_History
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHome));
+            this.lstHistory = new System.Windows.Forms.ListView();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstHistory
+            // 
+            this.lstHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstHistory.FullRowSelect = true;
+            this.lstHistory.GridLines = true;
+            this.lstHistory.Location = new System.Drawing.Point(10, 85);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(1423, 838);
+            this.lstHistory.TabIndex = 0;
+            this.lstHistory.UseCompatibleStateImageBehavior = false;
+            this.lstHistory.View = System.Windows.Forms.View.Details;
+            this.lstHistory.Click += new System.EventHandler(this.lstHistory_Click);
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHistory.Location = new System.Drawing.Point(12, 37);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.Size = new System.Drawing.Size(1421, 28);
+            this.txtHistory.TabIndex = 1;
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1445, 931);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.lstHistory);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmHome";
+            this.Text = "Clipboard History Manager";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstHistory;
+        private System.Windows.Forms.TextBox txtHistory;
+    }
+}
+
---
+++ Clipboard History/FrmHome.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Clipboard_History.DL;
+
+namespace Clipboard_History
+{
+    public partial class FrmHome : Form
+    {
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        IntPtr _nextClipboardViewer;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+
+        private readonly DataManager _dataManager;
+
+        public FrmHome()
+        {
+            InitializeComponent();
+            _dataManager = new DataManager();
+            _nextClipboardViewer = (IntPtr)SetClipboardViewer((int)Handle);
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            UpdateClipboardContent();
+        }
+
+        private void UpdateClipboardContent()
+        {
+
+            lstHistory.Clear();
+
+            var headersList = new List<string> { "Time", "Clipboard Content" };
+
+            lstHistory.Sorting = SortOrder.Descending;
+            lstHistory.View = View.Details;
+
+            lstHistory.Columns.Add(new ColumnHeader());
+            lstHistory.Columns[0].Text = headersList[0];
+            lstHistory.Columns[0].Width = 120;
+
+            lstHistory.Columns.Add(new ColumnHeader());
+            lstHistory.Columns[1].Text = headersList[1];
+            lstHistory.Columns[1].Width = lstHistory.Width - lstHistory.Columns[0].Width;
+
+            foreach (var clip in _dataManager.GetTodayClipboardContents())
+            {
+                string[] row = { clip.Time.ToString("HH:mm:ss"), clip.Content };
+                var listView = new ListViewItem(row);
+
+                if (lstHistory.Items.Count % 2 == 0)
+                {
+                    listView.BackColor = Color.Azure;
+                }
+
+                lstHistory.Items.Add(listView);
+            }
+
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // defined in winuser.h
+            const int wmDrawclipboard = 0x308;
+            const int wmChangecbchain = 0x030D;
+
+            switch (m.Msg)
+            {
+                case wmDrawclipboard:
+                    if (txtHistory.Text != Clipboard.GetText())
+                    {
+                        UpdateClipboardContent();
+                        _dataManager.StoreClipboardDataToFile();
+                        SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    }
+                    break;
+
+                case wmChangecbchain:
+
+                    if (m.WParam == _nextClipboardViewer)
+                        _nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        private void lstHistory_Click(object sender, EventArgs e)
+        {
+            txtHistory.Text = lstHistory.SelectedItems[0].SubItems[1].Text;
+        }
+    }
+}
