18e033cc762033ee47abb1c9006eddf83e43f040
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-07-11 12:43:35 +0100

    Add files via upload

---
+++ Clipboard History/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Clipboard History Manager")]
+[assembly: AssemblyDescription("Clipboard history maanger. Manage your daily clipboard contents")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Taiwo Kareem")]
+[assembly: AssemblyProduct("Clipboard History")]
+[assembly: AssemblyCopyright("Copyright ©Sycous Ltd. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8484bb98-c762-4de3-bea2-fd13655d4b14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
